7689b7d3f3bb8ef72b817c636f89ef1c0e080c3d
rockscripts <rockscripts@gmail.com>
2022-02-20 16:03:31 -0500

    add model files fixed v4 + payment_acquirer

--- cards_culqi/models/payment_acquirer.py
+++ cards_culqi/models/payment_acquirer.py
@@ -6,11 +6,11 @@ class payment_acquirer_culqi(models.Model):
 
     provider = fields.Selection(selection_add=[('culqi', 'Culqi')], ondelete={'culqi': 'cascade'})
 
-    culqi_public_key = fields.Char(string='Clave Publica')
-    culqi_private_key = fields.Char(string='Clave Privada')
+    culqi_public_key = fields.Char(string='Clave Publica para Pruebas')
+    culqi_private_key = fields.Char(string='Clave Privada para Pruebas')
 
-    culqi_public_key_produccion = fields.Char(string='Clave Publica')
-    culqi_private_key_produccion = fields.Char(string='Clave Privada')
+    culqi_public_key_produccion = fields.Char(string='Clave Publica para Producción')
+    culqi_private_key_produccion = fields.Char(string='Clave Privada para Producción')
 
     autoconfirm_invoice = fields.Boolean(string='¿Autoconfirmar factura?', default=True)
     autoconfirm_payment = fields.Boolean(string='¿Autoconfirmar pago contable?', default=True)
