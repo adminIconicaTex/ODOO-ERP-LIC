76192c41bea702a558796baf8cfd8e00f8687ec2
rockscripts <rockscripts@gmail.com>
2022-02-20 07:33:38 -0500

    add model files

--- cards_culqi/models/__init__.py
+++ cards_culqi/models/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from . import models
+from . import payment_acquirer
+from . import sale_order
+from . import payment_transaction
+from . import partner
+from . import account_move
