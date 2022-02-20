e93b2d6201a0839dcd567cd76c833788ca112c35
rockscripts <rockscripts@gmail.com>
2022-02-20 14:33:16 -0500

    add model files fixed v2 + payment_acquirer

--- cards_culqi/models/payment_acquirer.py
+++ cards_culqi/models/payment_acquirer.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-import json
-import sys
-import datetime
-from odoo.http import request
+from odoo import fields, models, _
 
 class payment_acquirer_culqi(models.Model):
     _inherit = 'payment.acquirer'    
 
     provider = fields.Selection(selection_add=[('culqi', 'Culqi')], ondelete={'culqi': 'cascade'})
-    # custom
+
     culqi_public_key = fields.Char(string='Clave Publica')
     culqi_private_key = fields.Char(string='Clave Privada')
 
