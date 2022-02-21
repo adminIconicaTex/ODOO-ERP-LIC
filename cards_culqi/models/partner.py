08d664fb0ab91a76ef0af344022d7841520307b6
rockscripts <rockscripts@gmail.com>
2022-02-21 04:36:21 -0500

    add model files fixed v8 + payment_acquirer

--- cards_culqi/models/partner.py
+++ cards_culqi/models/partner.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
-import datetime, sys, json
-from odoo.http import request
 
 class res_partner(models.Model):
     _inherit = 'res.partner'    
--- cards_culqi/models/sale_order.py
+++ cards_culqi/models/sale_order.py
@@ -6,20 +6,16 @@ class sale_order(models.Model):
 
     culqi_response = fields.Html( string="Culqi")
     culqi_json_response = fields.Text( string="JSON Culqi Response")
-    id_culqi = fields.Char("Culqi Order ID")
-
-    # Card Info
-    culqi_response = fields.Html(string="Culqi")
-    culqi_type = fields.Char(string="Tipo")
-    culqi_card_brand = fields.Char(string="Marca de Tarjeta")
-    culqi_card_type = fields.Char(string="Tipo de Tarjeta")
-    culqi_card_number = fields.Char(string="Número")
-    culqi_card_category = fields.Char(string="Categoria")
-
-    # Issuer
-    culqi_issuer_name =  fields.Char(string="Compañia emisora")
-    culqi_outcome_merchant_type = fields.Char(string="Mensaje para el vendedor")
-    culqi_outcome_merchant_message = fields.Char(string="Mensaje para el vendedor")
+    id_culqi = fields.Char( string="Culqi Order ID")
+    culqi_response = fields.Html( string="Culqi")
+    culqi_type = fields.Char( string="Tipo")
+    culqi_card_brand = fields.Char( string="Marca de Tarjeta")
+    culqi_card_type = fields.Char( string="Tipo de Tarjeta")
+    culqi_card_number = fields.Char( string="Número")
+    culqi_card_category = fields.Char( string="Categoria")
+    culqi_issuer_name =  fields.Char( string="Compañia emisora")
+    culqi_outcome_merchant_type = fields.Char( string="Mensaje para el vendedor")
+    culqi_outcome_merchant_message = fields.Char( string="Mensaje para el vendedor")
 
     def action_post(self):
         response = super(sale_order, self).action_post()
