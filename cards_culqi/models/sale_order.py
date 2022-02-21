df3c1a9cba25876ed448b029d9f08e4a560708d3
rockscripts <rockscripts@gmail.com>
2022-02-21 03:33:33 -0500

    add model files fixed v6 + payment_acquirer

--- cards_culqi/models/sale_order.py
+++ cards_culqi/models/sale_order.py
@@ -9,17 +9,17 @@ class sale_order(models.Model):
     id_culqi = fields.Char("Culqi Order ID")
 
     # Card Info
-    culqi_response = fields.Html( string="Culqi")
-    culqi_type = fields.Char("Tipo")
-    culqi_card_brand = fields.Char("Marca de Tarjeta")
-    culqi_card_type = fields.Char("Tipo de Tarjeta")
-    culqi_card_number = fields.Char("Número")
-    culqi_card_category = fields.Char("Categoria")
+    culqi_response = fields.Html(string="Culqi")
+    culqi_type = fields.Char(string="Tipo")
+    culqi_card_brand = fields.Char(string="Marca de Tarjeta")
+    culqi_card_type = fields.Char(string="Tipo de Tarjeta")
+    culqi_card_number = fields.Char(string="Número")
+    culqi_card_category = fields.Char(string="Categoria")
 
     # Issuer
-    culqi_issuer_name =  fields.Char("Compañia emisora")
-    culqi_outcome_merchant_type = fields.Char("Mensaje para el vendedor")
-    culqi_outcome_merchant_message = fields.Char("Mensaje para el vendedor")
+    culqi_issuer_name =  fields.Char(string="Compañia emisora")
+    culqi_outcome_merchant_type = fields.Char(string="Mensaje para el vendedor")
+    culqi_outcome_merchant_message = fields.Char(string="Mensaje para el vendedor")
 
     def action_post(self):
         response = super(sale_order, self).action_post()
